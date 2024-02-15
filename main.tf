552221d97d0b1bcb6ca44780c1c987260fd4735e
Pranav Prakash <prakash.pran@northeastern.edu>
2024-02-15 14:08:56 -0500

    Removed count implementation

--- main.tf
+++ main.tf
@@ -5,32 +5,30 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  count                           = var.number_of_vpcs
-  name                            = "${var.vpc_network_name}-${count.index + 1}"
+  name                            = var.vpc_network_name
   auto_create_subnetworks         = false
   routing_mode                    = var.vpc_routing_mode
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  count         = var.number_of_vpcs
-  name          = var.number_of_vpcs == "1" ? var.webapp_subnet_name : "${var.webapp_subnet_name}-${count.index + 1}"
+  name          = var.webapp_subnet_name
   ip_cidr_range = var.webapp_ipcidr
-  network       = google_compute_network.vpc_network[count.index].id
+  region        = var.webapp_region
+  network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "db" {
-  count         = var.number_of_vpcs
-  name          = var.number_of_vpcs == "1" ? var.db_subnet_name : "${var.db_subnet_name}-${count.index + 1}"
+  name          = var.db_subnet_name
   ip_cidr_range = var.db_ipcidr
-  network       = google_compute_network.vpc_network[count.index].id
+  region        = var.db_region
+  network       = google_compute_network.vpc_network.id
 
 }
 
 resource "google_compute_route" "route_internet" {
-  count            = var.number_of_vpcs
-  name             = "${var.route_internet_name}-${count.index + 1}"
+  name             = var.route_internet_name
   dest_range       = var.route_destination
   next_hop_gateway = var.route_internet_next_hop_gateway
-  network          = google_compute_network.vpc_network[count.index].id
+  network          = google_compute_network.vpc_network.id
 }

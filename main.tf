b5d71a4f9c6e685c9dbf530ae0a7f6a03aecd7ef
Pranav Prakash <prakash.pran@northeastern.edu>
2024-02-21 07:51:16 -0500

    Created VM and firewall rule

--- main.tf
+++ main.tf
@@ -5,34 +5,74 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  count                           = var.number_of_vpcs
-  name                            = "${var.vpc_network_name}-${count.index + 1}"
+  name                            = var.vpc_network_name
   auto_create_subnetworks         = false
   routing_mode                    = var.vpc_routing_mode
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  count         = var.number_of_vpcs
-  name          = count.index < "1" ? var.webapp_subnet_name : "${var.webapp_subnet_name}-${count.index + 1}"
+  name          = var.webapp_subnet_name
   ip_cidr_range = var.webapp_ipcidr
-  region        = var.webapp_region
-  network       = google_compute_network.vpc_network[count.index].id
+  network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "db" {
-  count         = var.number_of_vpcs
-  name          = count.index < "1" ? var.db_subnet_name : "${var.db_subnet_name}-${count.index + 1}"
+  name          = var.db_subnet_name
   ip_cidr_range = var.db_ipcidr
-  region        = var.db_region
-  network       = google_compute_network.vpc_network[count.index].id
+  network       = google_compute_network.vpc_network.id
 
 }
 
 resource "google_compute_route" "route_internet" {
-  count            = var.number_of_vpcs
-  name             = "${var.route_internet_name}-${count.index + 1}"
+  name             = var.route_internet_name
   dest_range       = var.route_destination
   next_hop_gateway = var.route_internet_next_hop_gateway
-  network          = google_compute_network.vpc_network[count.index].id
+  network          = google_compute_network.vpc_network.id
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = var.vm_tag
+
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = var.image
+      size  = var.disk_size
+      type  = var.disk_type
+    }
+
+  }
+  network_interface {
+    network    = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.webapp.self_link
+    access_config {
+      // Ephemeral public IP
+    }
+
+  }
+  service_account {
+    email  = var.service_account_email
+    scopes = var.service_account_scopes
+  }
+}
+
+resource "google_compute_firewall" "vm_instance_firewall" {
+  name        = var.firewall_name
+  network     = google_compute_network.vpc_network.self_link
+  description = var.firewall_description
+
+  allow {
+    protocol = var.firewall_protocol
+    ports    = var.firewall_ports
+  }
+  source_tags   = var.vm_tag
+  target_tags   = var.vm_tag
+  source_ranges = var.source_ranges
+}
+
+
--- variables.tf
+++ variables.tf
@@ -3,27 +3,10 @@ variable "project" {
   type        = string
 }
 
-variable "number_of_vpcs" {
-  description = "Number of vpcs to create"
-  type        = string
-}
-
-
 variable "region" {
   description = "The region where resources get deployed"
   type        = string
 }
-
-variable "webapp_region" {
-  description = "The region where webapp subnet get deployed"
-  type        = string
-}
-
-variable "db_region" {
-  description = "The region where db subnet get deployed"
-  type        = string
-}
-
 
 variable "vpc_routing_mode" {
   description = "Variable to define where the routes will be"
@@ -74,4 +57,70 @@ variable "route_internet_next_hop_gateway" {
   description = "The next hop gateway for the route to the internet"
   type        = string
 }
+
+variable "vm_instance_name" {
+  description = "The name of the VM instance"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "The machine type for the VM instance"
+  type        = string
+}
+
+variable "image" {
+  description = "The image for the boot disk"
+  type        = string
+}
+
+variable "vm_tag" {
+  description = "Tag to connect to firewall"
+  type        = list(string)
+}
+
+variable "disk_size" {
+  description = "The size of the boot disk in GB"
+  type        = number
+}
+
+variable "disk_type" {
+  description = "The type of the boot disk"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "The email of the service account"
+  type        = string
+}
+
+variable "service_account_scopes" {
+  description = "The scopes for the service account"
+  type        = list(string)
+}
+
+variable "firewall_description" {
+  description = "The description of the firewall rule"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "The name of the firewall created"
+  type        = string
+}
+
+variable "firewall_protocol" {
+  description = "The protocol to provide in firewall"
+  type        = string
+}
+
+variable "firewall_ports" {
+  description = "The ports to allow in the firewall rule"
+  type        = list(string)
+}
+
+variable "source_ranges" {
+  description = "The source IP ranges for the firewall rule"
+  type        = list(string)
+}
+
 
